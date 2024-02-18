48a72d4e3e3e72c5777dc0ee42adb0a5077fadc5
romgil <gille.romain@yahoo.fr>
2024-02-18 15:22:20 +0100

    working solution

--- Test_clipboardPlugin/src/Test_clipboardPlugin/Actions/CounterAdjustment.cs
+++ Test_clipboardPlugin/src/Test_clipboardPlugin/Actions/CounterAdjustment.cs
@@ -1,25 +1,47 @@
 namespace Loupedeck.Test_clipboardPlugin
 {
     using System;
+    using Melanchall.DryWetMidi.Core;
+    using Melanchall.DryWetMidi.Multimedia;
 
     // This class implements an example adjustment that counts the rotation ticks of a dial.
 
     public class CounterAdjustment : PluginDynamicAdjustment
     {
         // This variable holds the current value of the counter.
-        private Int32 _counter = 0;
+        private Byte _counter = 0;
+        OutputDevice outputDeviceNew;
 
         // Initializes the adjustment class.
         // When `hasReset` is set to true, a reset command is automatically created for this adjustment.
         public CounterAdjustment()
             : base(displayName: "Tick Counter", description: "Counts rotation ticks", groupName: "Adjustments", hasReset: true)
         {
+            this.outputDeviceNew = OutputDevice.GetByName("midi1");
+
         }
+
+         ~CounterAdjustment() { this.outputDeviceNew.Dispose(); }
+
 
         // This method is called when the adjustment is executed.
         protected override void ApplyAdjustment(String actionParameter, Int32 diff)
         {
-            this._counter += diff; // Increase or decrease the counter by the number of ticks.
+            if(this._counter + diff<0)
+            {this._counter =0;} else if
+             (this._counter + diff > 127)
+            { this._counter =127;} else
+            { this._counter += (byte)diff; }
+
+            // Increase or decrease the counter by the number of ticks.
+            
+                this.outputDeviceNew.SendEvent(
+                new ControlChangeEvent(
+                    new Melanchall.DryWetMidi.Common.SevenBitNumber(1),
+                    new Melanchall.DryWetMidi.Common.SevenBitNumber(this._counter)
+                    )
+                );
+            
             this.AdjustmentValueChanged(); // Notify the Loupedeck service that the adjustment value has changed.
         }
 
--- Test_clipboardPlugin/src/Test_clipboardPlugin/Actions/CounterCommand.cs
+++ Test_clipboardPlugin/src/Test_clipboardPlugin/Actions/CounterCommand.cs
@@ -1,18 +1,17 @@
 namespace Loupedeck.Test_clipboardPlugin
 {
     using System;
-    using Loupedeck.Midi;
     using Loupedeck;
+    using Melanchall.DryWetMidi.Multimedia;
+    using Melanchall.DryWetMidi.Core;
 
     // This class implements an example command that counts button presses.
 
     public class CounterCommand : PluginDynamicCommand
+
     {
-        ActionEditor instance = new ActionEditor();
-        EverySecondTick newInst = new EverySecondTick(2);
-        LoupedeckDeviceAttribute tests = new LoupedeckDeviceAttribute(Loupedeck.DeviceType.Loupedeck20);
-        private readonly string myStr = "Ok";
-        private Int32 _counter = 2;
+        private Int32 _counter = 0;
+
         // Initializes the command class.
         public CounterCommand()
             : base(displayName: "Press Counter", description: "Counts button presses", groupName: "Commands")
@@ -22,19 +21,28 @@ namespace Loupedeck.Test_clipboardPlugin
         // This method is called when the user executes the command.
         protected override void RunCommand(String actionParameter)
         {
-            int myNumber = 10;
-            this.newInst.UpdateTicks(ref myNumber);
-            this.Plugin.KeyboardApi.SendShortcut("c");
-            this.Plugin.ExecuteGenericAction("SendShortcut", "d", 3);
-            this.Plugin.ExecuteGenericAction("MidiNotePlay", "2",1);
-            this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.VolumeMute);
             this._counter++;
+
+            using (var outputDevice = OutputDevice.GetByName("midi1"))
+            {
+                outputDevice.SendEvent(
+                new NoteOnEvent(
+                    new Melanchall.DryWetMidi.Common.SevenBitNumber(2),
+                    new Melanchall.DryWetMidi.Common.SevenBitNumber(127)
+                    )
+                );
+                outputDevice.SendEvent(
+                new NoteOffEvent(
+                    new Melanchall.DryWetMidi.Common.SevenBitNumber(2),
+                    new Melanchall.DryWetMidi.Common.SevenBitNumber(127)
+                    )
+                );
+            }
             this.ActionImageChanged(); // Notify the Loupedeck service that the command display name and/or image has changed.
-            PluginLog.Info($"Counter value is {this._counter}"); // Write the current counter value to the log file.
         }
 
         // This method is called when Loupedeck needs to show the command on the console or the UI.
         protected override String GetCommandDisplayName(String actionParameter, PluginImageSize imageSize) =>
-            $"Press1 {this.myStr}  Counter{Environment.NewLine}{this._counter}";
+            $"  Counter{Environment.NewLine}{this._counter}";
     }
 }
